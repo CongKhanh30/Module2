233513381fdcbb8e5af6ca6c23adb69bae0ef9bb
CongKhanh30 <khanhnc45@wru.vn>
2023-06-23 09:38:37 +0700

    Triển khai Queue sử dụng danh sách liên kết

---
+++ src/section11/exercise2/Node.java
@@ -0,0 +1,11 @@
+package section11.exercise2;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}

837f79ff6cb0904905eabc7d3c9750c5610750e3
CongKhanh30 <khanhnc45@wru.vn>
2023-06-23 15:42:29 +0700

    Sắp xếp với Comparable và Comparator

---
+++ src/section12/exercise3/AgeComparator.java
@@ -0,0 +1,10 @@
+package section12.exercise3;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getAge() - o2.getAge();
+    }
+}

22a79f357e4d08c4246e6a5e1e061b561339dbff
CongKhanh30 <khanhnc45@wru.vn>
2023-06-28 10:08:52 +0700

    update

--- src/practice2/studentManagement/ReaderAndWriteStudent.java
+++ src/practice2/studentManagement/ReaderAndWriteStudent.java
@@ -11,10 +11,10 @@ public class ReaderAndWriteStudent {
             br = new BufferedReader(new FileReader("src/practice2/studentManagement/data.csv"));
             List<Student> data = new ArrayList<>();
             String[] lineData;
-            String line ="";
-            while ((line = br.readLine()) != null){
+            String line = "";
+            while ((line = br.readLine()) != null) {
                 lineData = line.split(",");
-                Student student = new Student( Integer.parseInt(lineData[0]), lineData[1], Integer.parseInt(lineData[2]), lineData[3]);
+                Student student = new Student(Integer.parseInt(lineData[0]), lineData[1], Integer.parseInt(lineData[2]), lineData[3]);
                 data.add(student);
             }
             br.close();
@@ -25,13 +25,13 @@ public class ReaderAndWriteStudent {
             throw new RuntimeException(e);
         }
     }
-    public void writeFile(List<Student> list){
+
+    public void writeFile(List<Student> list) {
         BufferedWriter bw;
         try {
             bw = new BufferedWriter(new FileWriter("src/practice2/studentManagement/data.csv"));
-            for (Student s :
-                    list) {
-                bw.write(s.getId() +","+s.getFullName()+","+s.getAge()+","+s.getHometown()+"\n");
+            for (Student s : list) {
+                bw.write(s.getId() + "," + s.getFullName() + "," + s.getAge() + "," + s.getHometown() + "\n");
             }
             bw.close();
 
